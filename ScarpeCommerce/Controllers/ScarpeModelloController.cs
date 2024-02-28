1df4e3e177da057e01f696afb2a39db44df4f310
Mariline Luccisano <y.marilineluccisano@outlook.it>
2024-02-28 17:08:59 +0100

    Aggiungere i file di progetto.

---
+++ ScarpeCommerce/Controllers/ScarpeModelloController.cs
@@ -0,0 +1,74 @@
+﻿// ScarpeModelloController.cs
+
+using System;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.IO;
+using System.Web;
+using System.Web.Mvc;
+using ScarpeCommerce.Models;
+
+namespace ScarpeCommerce.Controllers
+{
+    public class ScarpeModelloController : Controller
+    {
+        // ... altri metodi del controller
+
+
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(ScarpeModello model, HttpPostedFileBase Copertina, HttpPostedFileBase AltreImg1, HttpPostedFileBase AltreImg2)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["ConnectionScarpe"].ConnectionString.ToString();
+            SqlConnection conn = new SqlConnection(connectionString);
+
+            try
+            {
+                conn.Open();
+
+                // Salvataggio delle immagini sul server
+                if (Copertina != null && Copertina.ContentLength > 0)
+                {
+                    string _File = Path.GetFileName(Copertina.FileName);
+                    string _path = Path.Combine(Server.MapPath("~/UploadedFiles"), _File);
+                    Copertina.SaveAs(_path);
+                    
+                }
+
+                
+                string query = "INSERT INTO Scarpe (Nome, Descrizione , Copertina, Prezzo, Imm1, Imm2, Disponibile) " +
+                               "VALUES (@NomeProdotto, @Prezzo, @DescrizioneDettagliata, @Copertina, @AltreImg1, @AltreImg2, @Disponibile)";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@NomeProdotto", model.Nome);
+                cmd.Parameters.AddWithValue("@Prezzo", model.Prezzo);
+                cmd.Parameters.AddWithValue("@DescrizioneDettagliata", model.Descrizione);
+                cmd.Parameters.AddWithValue("@Copertina", model.Copertina);
+           
+                cmd.Parameters.AddWithValue("@Disponibile", model.Disponibile);
+
+                cmd.ExecuteNonQuery();
+            }
+            catch (SqlException ex)
+            {
+                System.Diagnostics.Debug.WriteLine("Errore nella richiesta SQL");
+                ViewBag.Message = "Si è verificato un errore di connessione durante l'inserimento: " + ex.Message;
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            return RedirectToAction("Index", "ScarpeModello");
+        }
+    }
+}
---
+++ ScarpeCommerce/Models/ScarpeModello.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ScarpeCommerce.Models
+{
+    public class ScarpeModello
+    {
+        public int ID_Prodotto { get; set; }
+        public string Nome { get; set; }
+        public string Descrizione { get; set; }
+     
+        public HttpPostedFileBase Copertina { get; set; }
+        public string Prezzo { get; set; }
+   
+        public HttpPostedFileBase AltreImg1File { get; set; }
+        public HttpPostedFileBase AltreImg2File { get; set; }
+
+        public bool Disponibile { get; set; }
+
+        public ScarpeModello() { }
+
+       public ScarpeModello(int iD_Prodotto, string nome, string descrizione, HttpPostedFileBase copertina, string prezzo, HttpPostedFileBase altreImg1File, HttpPostedFileBase altreImg2File, bool disponibile)
+        {
+            ID_Prodotto = iD_Prodotto;
+            Nome = nome;
+            Descrizione = descrizione;
+            Copertina = copertina;
+            Prezzo = prezzo;
+            AltreImg1File = altreImg1File;
+            AltreImg2File = altreImg2File;
+            Disponibile = disponibile;
+        }
+    }
+}
